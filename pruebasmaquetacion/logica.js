45b9c5f9308fb0df0c47b699b8fa87923d5bd966
alexissena2024 <acavanzo@soy.sena.edu.co>
2024-07-01 20:18:30 -0500

    ejercicio de niveles de agrupamiento objetos

---
+++ pruebasmaquetacion/logica.js
@@ -0,0 +1,28 @@
+let dataPersonas=[
+    {id:1,nombre:'oscar',edad:20},
+    {id:2,nombre:'perez',edad:30},
+    {id:3,nombre:'juan',edad:50}
+]
+let dataPersonasJSON=[
+    {
+        "nombre plato":"frijoles",
+        "precio":20000,
+        "chef":"JULIAN",
+        "INGREDIENTES":[
+            {"nombre":"Arroz","cantidad":400,"precio":2000},
+            {"nombre":"Pollo","cantidad":300,"precio":7000}
+        ],
+        "acompañantes":[{
+           "bebidas":[{"frias":[],"calientes":[]} ],
+           "otro":[]
+        }
+            
+        ],
+    },
+]
+
+            
+                
+               
+                
+        
